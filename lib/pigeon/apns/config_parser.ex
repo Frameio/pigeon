6b67b62aee20c6c755c35a319751ff882ebb2b5a
Michael Guarino <mguarino@frame.io>
2018-06-29 13:06:20 -0400

    fix file_path bug

--- lib/pigeon/apns/config_parser.ex
+++ lib/pigeon/apns/config_parser.ex
@@ -43,11 +43,10 @@ defmodule Pigeon.APNS.ConfigParser do
 
   @doc false
   def file_path(nil), do: nil
-
   def file_path(path) when is_binary(path) do
     if :filelib.is_file(path), do: Path.expand(path), else: nil
   end
-
+  def file_path({:system, _}), do: nil
   def file_path({app_name, path}) when is_atom(app_name),
     do: Path.expand(path, :code.priv_dir(app_name))
 
